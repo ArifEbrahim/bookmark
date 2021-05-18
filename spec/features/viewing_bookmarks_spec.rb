263fe473bdb0809960dcc033020c669a692b242e
Arif Ebrahim <>
2021-05-18 19:07:18 +0100

    fixed typo

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -2,9 +2,9 @@ require 'pg'
 
 feature 'viewing bookmarks' do
   scenario 'user can see saved bookmarks' do
-    Bookmark.create(url: "http://www.makersacademy.com")
-    Bookmark.create(url: "http://www.amazon.com")
-    Bookmark.create(url: "http://www.google.com")
+    Bookmark.create("http://www.makersacademy.com")
+    Bookmark.create("http://www.amazon.com")
+    Bookmark.create("http://www.google.com")
 
     visit('/')
     click_button('View')
