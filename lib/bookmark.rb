36f4ee416dec2bfc24f1c701aebed69da6eee09e
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-05-20 18:43:48 +0100

    completed step 14

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,68 +1,34 @@
-require 'pg'
+require './lib/database_connection.rb'
 
 class Bookmark
 
   attr_reader :id, :title, :url
 
   def initialize(id, title, url) 
-    @id = id
-    @title = title 
-    @url = url
+    @id, @title, @url = id, title, url
   end 
 
   def self.all
-    if ENV['ENVIRONMENT'] == 'test'
-      con = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      con = PG.connect(dbname: 'bookmark_manager')
-    end
-
-    results = con.exec( 'SELECT * FROM bookmarks;' )
-    results.map do |bookmark|
-      Bookmark.new(bookmark['id'], bookmark['title'], bookmark['url'])
-    end
+    results = DatabaseConnection.query( 'SELECT * FROM bookmarks;' )
+    results.map { |bookmark| Bookmark.new(bookmark['id'], bookmark['title'], bookmark['url']) }
   end
 
   def self.create(url, title)
-    if ENV['ENVIRONMENT'] == 'test'
-      con = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      con = PG.connect(dbname: 'bookmark_manager')
-    end
-
-    result = con.exec("INSERT INTO bookmarks (url, title) VALUES ('#{url}', '#{title}') RETURNING id, url, title")
+    result = DatabaseConnection.query("INSERT INTO bookmarks (url, title) VALUES ('#{url}', '#{title}') RETURNING id, url, title")
     Bookmark.new(result[0]['id'], result[0]['title'], result[0]['url'])
   end
 
   def self.delete(id)
-    if ENV['ENVIRONMENT'] == 'test'
-      con = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      con = PG.connect(dbname: 'bookmark_manager')
-    end
-
-    con.exec("DELETE FROM bookmarks WHERE id = #{'id'}")
+    DatabaseConnection.query("DELETE FROM bookmarks WHERE id = #{'id'}")
   end
 
   def self.update(url, title, id)
-    if ENV['ENVIRONMENT'] == 'test'
-      con = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      con = PG.connect(dbname: 'bookmark_manager')
-    end
-
-    result = con.exec("UPDATE bookmarks SET url = '#{url}', title = '#{title}' WHERE id = '#{id}' RETURNING id, url, title")
+    result = DatabaseConnection.query("UPDATE bookmarks SET url = '#{url}', title = '#{title}' WHERE id = '#{id}' RETURNING id, url, title")
     Bookmark.new(result[0]['id'], result[0]['title'], result[0]['url'])
   end
 
   def self.find(id)
-    if ENV['ENVIRONMENT'] == 'test'
-      con = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      con = PG.connect(dbname: 'bookmark_manager')
-    end
-
-    result = con.exec("SELECT * FROM bookmarks WHERE id = #{id};")
+    result = DatabaseConnection.query("SELECT * FROM bookmarks WHERE id = #{id};")
     Bookmark.new(result[0]['id'], result[0]['title'], result[0]['url'])
   end
 
